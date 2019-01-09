dcc1a28b7f61197e3d2d60830b0417bc6f8f244c
Ignas Dailydė <ignas.dailyde@gmail.com>
2019-01-09 13:47:53 +0200

    login and logout

--- src/App.js
+++ src/App.js
@@ -10,17 +10,69 @@ import PetTypeCreate from './components/PetType/PetTypeCreate'
 import UserIndex from './components/User/UserIndex'
 import UserCreate from './components/User/UserCreate'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import axios from "axios";
+import config from "./config";
+
 
 class App extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            email: '',
+            password: ''
+        }
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+
+    componentDidMount() {
+        let jwt = localStorage.getItem('jwt');
+        if (jwt) {
+            this.setState({loggedIn: true});
+        } else {
+            this.setState({loggedIn: false});
+        }
+
+        this.setAuthorizationToken(jwt);
+    }
+
+    setAuthorizationToken(token) {
+        if(token) {
+            axios.defaults.headers.common['authorization'] = `Bearer ${token}`;
+        } else {
+            delete axios.defaults.headers.common['authorization'];
+        }
+    }
+
+    login() {
+        axios.post(`http://${config.FETCH_URL}/api/v1/authenticate`, {
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(response => {
+                localStorage.setItem('jwt', response.data.token);
+                this.setState({loggedIn: true})
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    logout() {
+        this.setAuthorizationToken();
+        localStorage.removeItem('jwt');
+        this.setState({loggedIn: false});
+
+    }
+
     render() {
 
-
-        const Shelters = () => <h2>About</h2>;
-        const Users = () => <h2>Users</h2>;
-        const Pets = () => <h2>Pets</h2>;
-
         return (
-            <Router>
+            this.state.loggedIn ?
+                <Router>
                 <div className="App">
 
                     <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -56,9 +108,19 @@ class App extends Component {
                             </div>
 
                         </div>
+
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                <div className="buttons">
+                                    <button onClick={this.logout} className="button is-light">
+                                        Log out
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </nav>
 
-                    <Route path="/" exact component={Shelters}/>
+                    <Route path="/" exact component={UserIndex}/>
                     <Route path="/users/" exact component={UserIndex}/>
                     <Route path="/users/create/" exact component={UserCreate}/>
                     <Route path="/shelters/" exact component={ShelterIndex}/>
@@ -70,7 +132,29 @@ class App extends Component {
 
 
                 </div>
-            </Router>
+            </Router>  :  <div>
+                    <div className="field">
+                        <label className="label">Email</label>
+                        <div className="control">
+                            <input className="input" type="text" placeholder="Email" onChange={event => {this.setState({email: event.target.value})}}/>
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <label className="label">Password</label>
+                        <div className="control">
+                            <input className="input" type="password" placeholder="Password" onChange={event => {this.setState({password: event.target.value})}}/>
+                        </div>
+                    </div>
+
+                    <div className="field">
+                        <div className="control">
+                            <button className="button is-link" onClick={this.login}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+
+
         );
     }
 }
